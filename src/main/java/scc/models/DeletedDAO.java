501ddd818e6a0a9f8001473df2287368bc25c7ac
Pedro Dimas <pedro.m.dimas@gmail.com>
2021-11-30 21:18:05 +0000

    help

--- src/main/java/scc/models/DeletedDAO.java
+++ src/main/java/scc/models/DeletedDAO.java
@@ -1,10 +1,17 @@
 package scc.models;
 
-import lombok.Data;
 
-@Data
 public class DeletedDAO {
     private String _rid;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     private String _ts;
     private String id;
 }
